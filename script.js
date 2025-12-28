1d3e1bc82620417689eb28df222538e2e5cbee17
Unity <guilhermtrout@gmail.com>
2025-12-28 15:58:08 -0300

    Ajuste no audio

--- script.js
+++ script.js
@@ -4,15 +4,26 @@ const bgMusic = document.getElementById("bg-music");
 let index = 0;
 let musicaIniciada = false;
 
-function iniciarMusica() {
+window.addEventListener("load", () => {
+  if (!bgMusic) return;
+
+  bgMusic
+    .play()
+    .then(() => {
+      musicaIniciada = true;
+    })
+    .catch(() => {});
+});
+
+function liberarAudio() {
   if (!musicaIniciada && bgMusic) {
     bgMusic.play().catch(() => {});
     musicaIniciada = true;
   }
 }
 
-document.addEventListener("click", iniciarMusica, { once: true });
-document.addEventListener("touchstart", iniciarMusica, { once: true });
+document.addEventListener("click", liberarAudio, { once: true });
+document.addEventListener("touchstart", liberarAudio, { once: true });
 
 const interval = setInterval(() => {
   items[index].classList.remove("active");
